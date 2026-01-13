43dbbb76bc8c525f89f87cc01388b4f5dff44d09
Shubham Pachchigar <spachchigar@horizontal.com>
2026-01-13 15:27:58 +0530

    Added jstag send event on form submission

--- components/section-with-html-code.tsx
+++ components/section-with-html-code.tsx
@@ -1,4 +1,4 @@
-import parse from 'html-react-parser';
+import parse from "html-react-parser";
 
 type AdditionalParam = {
   title: string;
@@ -8,7 +8,7 @@ type AdditionalParam = {
   html_code: string;
   designation: string;
   name: string;
-}
+};
 
 type ObjectProps = {
   html_code_alignment: string;
@@ -16,41 +16,78 @@ type ObjectProps = {
   description: string;
   html_code: string;
   $: AdditionalParam;
-}
+};
 
-export default function SectionWithHtmlCode({ embedCode }: {embedCode : ObjectProps}) {
-  if (embedCode.html_code_alignment === 'Left') {
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  const formData = new FormData(e.target as HTMLFormElement);
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  const message = formData.get("message") as string;
+
+  if (typeof window !== "undefined") {
+    const jstagInstance = (window as any).jstag;
+    jstagInstance.send({
+      name,
+      email,
+    });
+  }
+
+  console.log(name, email, message);
+  alert("Form submitted");
+  (e.target as HTMLFormElement).reset();
+};
+
+export default function SectionWithHtmlCode({
+  embedCode,
+}: {
+  embedCode: ObjectProps;
+}) {
+  if (embedCode.html_code_alignment === "Left") {
     return (
-      <div className='contact-page-section max-width'>
-        <div className='contact-page-content'>
+      <div className="contact-page-section max-width">
+        <div className="contact-page-content">
           {embedCode.title && (
-            <h1 {...embedCode.$?.title as {}}>{embedCode.title}</h1>
+            <h1 {...(embedCode.$?.title as {})}>{embedCode.title}</h1>
           )}
-          {typeof embedCode.description === 'string' && (
-            <div {...embedCode.$?.description as {}}>
+          {typeof embedCode.description === "string" && (
+            <div {...(embedCode.$?.description as {})}>
               {parse(embedCode.description)}
             </div>
           )}
         </div>
-        <div className='contact-page-form'>
-          {typeof embedCode.html_code === 'string' && (
-            <div {...embedCode.$?.html_code as {}}>{parse(embedCode.html_code)}</div>
-          )}
+        <div className="contact-page-form">
+          <form onSubmit={handleSubmit}>
+            <div className="input-fields">
+              <input placeholder="Name*" />
+            </div>
+            <div className="input-fields">
+              <input placeholder="Email*" />
+            </div>
+            <div className="text-field">
+              <input type="text-area" placeholder="Message*" />
+            </div>
+            <button type="submit" className="btn primary-btn">
+              Send Message
+            </button>
+          </form>
         </div>
       </div>
     );
   }
   return (
-    <div className='contact-maps-section max-width'>
-      <div className='maps-details'>
-        {typeof embedCode.html_code === 'string' && (
-          <div {...embedCode.$?.html_code as {}}>{parse(embedCode.html_code)}</div>
+    <div className="contact-maps-section max-width">
+      <div className="maps-details">
+        {typeof embedCode.html_code === "string" && (
+          <div {...(embedCode.$?.html_code as {})}>
+            {parse(embedCode.html_code)}
+          </div>
         )}
       </div>
-      <div className='contact-maps-content'>
-        {embedCode.title ? <h2>{embedCode.title}</h2> : ''}
-        {typeof embedCode.description === 'string' && (
-          <div {...embedCode.$?.description as {}}>
+      <div className="contact-maps-content">
+        {embedCode.title ? <h2>{embedCode.title}</h2> : ""}
+        {typeof embedCode.description === "string" && (
+          <div {...(embedCode.$?.description as {})}>
             {parse(embedCode.description)}
           </div>
         )}
