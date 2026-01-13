ccf5ff8ce1df3679a2a95ef07c1ddf06e0f07c4f
Shubham Pachchigar <spachchigar@horizontal.com>
2026-01-13 11:03:46 +0530

    Initial Commit

---
+++ pages/_document.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable react/react-in-jsx-scope */
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const lyticsScript = `!function(){"use strict";var o=window.jstag||(window.jstag={}),r=[];function n(e){o[e]=function(){for(var n=arguments.length,t=new Array(n),i=0;i<n;i++)t[i]=arguments[i];r.push([e,t])}}n("send"),n("mock"),n("identify"),n("pageView"),n("unblock"),n("getid"),n("setid"),n("loadEntity"),n("getEntity"),n("on"),n("once"),n("call"),o.loadScript=function(n,t,i){var e=document.createElement("script");e.async=!0,e.src=n,e.onload=t,e.onerror=i;var o=document.getElementsByTagName("script")[0],r=o&&o.parentNode||document.head||document.body,c=o||r.lastChild;return null!=c?r.insertBefore(e,c):r.appendChild(e),this},o.init=function n(t){return this.config=t,this.loadScript(t.src,function(){if(o.init===n)throw new Error("Load error!");o.init(o.config),function(){for(var n=0;n<r.length;n++){var t=r[n][0],i=r[n][1];o[t].apply(o,i)}r=void 0}()}),this}}();
+  // Define config and initialize Lytics tracking tag.
+  // - The setup below will disable the automatic sending of Page Analysis Information (to prevent duplicative sends, as this same information will be included in the jstag.pageView() call below, by default)
+  jstag.init({
+    src: 'https://c.lytics.io/api/tag/9718d7603eb2bcb04fe6375208ccacf8/latest.min.js'
+  });
+  
+  // You may need to send a page view, depending on your use-case
+  jstag.pageView();`;
+    return (
+      <Html lang="en">
+        <Head>
+          <Script
+            id="lytics-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{ __html: lyticsScript }}
+          ></Script>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Inter&amp;display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+            integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossOrigin="anonymous"
+          />
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossOrigin="anonymous"
+            defer
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link href="/favicon.ico" rel="icon" type="image/ico" sizes="16x16" />
+          <link rel="apple-touch-icon" href="/path/to/apple-touch-icon.png" />
+          <meta name="theme-color" content="#317EFB" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
